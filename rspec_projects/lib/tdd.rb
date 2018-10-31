5e3f9d3db99da3cdc912f904cf3ab03da16c5d0d
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-31 14:28:59 -0700

    Create test suite for tdd.rb

--- rspec_projects/lib/tdd.rb
+++ rspec_projects/lib/tdd.rb
@@ -1,8 +1,9 @@
 class TDD
-attr_reader :arr
+attr_reader :arr, :matrix 
 
-  def initialize 
-    @arr = []
+  def initialize(arr = [], matrix = [[0,1,2],[3,4,5],[6,7,8]])
+    @arr = arr
+    @matrix = matrix
   end 
 
   def my_unique(arr)
@@ -12,5 +13,41 @@ attr_reader :arr
     end 
     result
   end 
+  
+  def two_sum
+    result = []
+    @arr.each_with_index do |nums, i|
+      @arr.each_with_index do |nums2, j|
+        result << [i,j] if j > i && nums + nums2 == 0
+      end 
+    end 
+    result 
+  end 
+  
+  def my_transpose
+    rows = @matrix.length 
+    cols = @matrix[0].length 
+    result = Array.new(cols) { Array.new(rows)}
+    
+    (0...rows).each do |i|
+      (0...cols).each do |j|
+        result[i][j] = @matrix[j][i]
+      end 
+    end 
+    result
+  end 
+  
+  def stock_picker(prices)
+    hsh = {}
+    
+    prices.each_with_index do |el, i|
+      prices.each_with_index do |el2, j|
+        hsh[el2 - el] = [i, j] if j > i
+      end
+    end
+    
+    hsh = (hsh.sort_by { |k, v| k }).to_h
+    hsh.values[-1]
+  end
   
 end
--- rspec_projects/spec/tdd_spec.rb
+++ rspec_projects/spec/tdd_spec.rb
@@ -3,12 +3,47 @@ require 'rspec'
 
 
 describe TDD do 
+
   subject (:test) { TDD.new }
+  
   describe "#initialize" do 
     it "will create an empty array" do 
       expect(test.arr).to eq([])
     end 
-  
   end 
+  
+  subject (:test2) { TDD.new([-1,2,1,3,-3]) }
+  
+  describe "#my_unique" do 
+    it "will remove duplicates from array" do 
+      expect(test2.my_unique(test2.arr)).to eq(test2.arr.uniq)
+    end 
+  end 
+    
+  describe "#two_sum" do 
+    it "will return indices of pairs summed to zero" do 
+      expect(test2.two_sum).to eq([[0,2],[3,4]])
+    end 
+  end 
+  
+  describe "#my_transpose" do 
+    it "will take the transpose of a matrix" do 
+      expect(test2.my_transpose).to eq(test2.matrix.transpose)
+    end
+  end 
+  
+  describe "#stock_picker" do 
+    prices = [3,4,6,2,1,3]
+    
+    it "will return the most profitable pair of days" do 
+      expect(test2.stock_picker(prices)).to eq([0,2])
+      
+    end 
+  end 
+  
+  
+  
+  
+  
   
 end 
